12d55acd11815352c8c8db8435acfe07111d98bb
Arshit790 <arshitdixit5@gmail.com>
2024-11-05 14:54:10 +0530

    "adding one more same elements like child1,child2"

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 // Nested Elements
 // For passing siblings => we have to pass the array of childrens in 3rd parameter
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "Hi I'm h1 Tag!"),
     React.createElement("h2", {}, "Hi I'm h2 Tag!"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hi I'm h1 Tag!"),
+    React.createElement("h2", {}, "Hi I'm h2 Tag!"),
+  ]),
+]);
 
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
