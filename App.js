77547ccad8a64856661e6cf46b04ce86d810ac37
Arshit790 <arshitdixit5@gmail.com>
2024-11-05 14:46:44 +0530

    "Passing sibling using arrays of children"

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 // Nested Elements
-
+// For passing siblings => we have to pass the array of childrens in 3rd parameter
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hi I'm h1 Tag!")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hi I'm h1 Tag!"),
+    React.createElement("h2", {}, "Hi I'm h2 Tag!"),
+  ])
 );
 
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
