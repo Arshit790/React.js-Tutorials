36d355be1e2e39c7c42e4d0e75590131bbae0685
Arshit790 <arshitdixit5@gmail.com>
2024-11-09 10:41:13 +0530

    "adding react and dom using npm"

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,23 @@
+// Nested Elements
+// For passing siblings => we have to pass the array of childrens in 3rd parameter
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "Hi I'm h1 Tag!"),
+        React.createElement("h2", {}, "Hi I'm h2 Tag!")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "Hi I'm h1 Tag!"),
+        React.createElement("h2", {}, "Hi I'm h2 Tag!")
+    ])
+]);
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
